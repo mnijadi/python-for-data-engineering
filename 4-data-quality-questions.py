cccf86264c0498d9106a6090dfa67ca6aace9eca
Mohamed Nijadi <mohammednijadi47@gmail.com>
2024-10-15 21:19:52 +0000

    chore: complete data quality assignment

--- 4-data-quality-questions.py
+++ 4-data-quality-questions.py
@@ -7,4 +7,7 @@ df = pl.read_csv("./data/sample_data.csv")
 # Question: Check for Nulls on column Id and that Customer_ID column is unique
 # check docs at https://canimus.github.io/cuallee/polars/ on how to define a check and run it.
 # you will end up with a dataframe of results, check that the `status` column does not have any "FAIL" in it
-
+check = Check()
+checks_df = check.is_complete("Customer_ID").is_unique("Customer_ID").validate(df)
+if "FAIL" not in checks_df["status"]:
+    print("Checks are complete successfully")
