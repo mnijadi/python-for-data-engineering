9d0f197c28297ff1c14e0febfa4f3887ce5cb69a
Mohamed Nijadi <mohammednijadi47@gmail.com>
2024-10-15 21:31:36 +0000

    chore: complete unit testing assingment

--- tests/test_cleaning_functions-questions.py
+++ tests/test_cleaning_functions-questions.py
@@ -3,8 +3,15 @@ from cleaning_functions import remove_duplicates
 
 def test_remove_duplicates():
     # Define sample data and unique_key
-    data = []
-    unique_key = ""
+    data = [
+        {"id": 1, "name": "user_1"},
+        {"id": 2, "name": "user_2"},
+        {"id": 3, "name": "user_3"},
+        {"id": 3, "name": "user_4"},
+        {"id": 4, "name": "user_5"},
+        {"id": 2, "name": "user_2"},
+    ]
+    unique_key = "id"
 
     # Call the function
     unique_data = remove_duplicates(data, unique_key)
@@ -13,6 +20,10 @@ def test_remove_duplicates():
     # assert len(unique_data) == some number based on your input
     # Assert the actual values
     expected_data = [
+        {"id": 1, "name": "user_1"},
+        {"id": 2, "name": "user_2"},
+        {"id": 3, "name": "user_3"},
+        {"id": 4, "name": "user_5"},
     ]
     assert unique_data == expected_data
 
