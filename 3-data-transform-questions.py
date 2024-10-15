2c2675250715d3b52952b22d9f7904374c5a4629
Mohamed Nijadi <mohammednijadi47@gmail.com>
2024-10-15 18:07:56 +0000

    chore: complete transform assignment

--- 3-data-transform-questions.py
+++ 3-data-transform-questions.py
@@ -93,24 +93,123 @@ print(
 
 # Question: How do you connect to DuckDB and load data from a CSV file into a DuckDB table?
 # Connect to DuckDB and load data
+import duckdb
+
+duckdb_conn = duckdb.connect(database=":memory:", read_only=False)
 
 # Read data from CSV file into DuckDB table
+duckdb_conn.execute("""CREATE TABLE sample_data (
+    Customer_ID INTEGER,
+    Customer_Name VARCHAR,
+    Age INTEGER,
+    Gender VARCHAR,
+    Purchase_Amount FLOAT,
+    Purchase_Date DATE
+)""")
+
+duckdb_conn.execute(f"COPY sample_data FROM '{csv_file_location}' (AUTO_DETECT TRUE);")
 
 # Question: How do you remove duplicate rows based on customer ID in DuckDB?
+duplicates_removed_query = """
+CREATE OR REPLACE TABLE duplicates_removed AS
+SELECT
+    *,
+    ROW_NUMBER()
+    OVER (
+        PARTITION BY customer_id
+    ) AS duplicate_number
+FROM sample_data
+ORDER BY customer_id;
+DELETE FROM duplicates_removed
+WHERE duplicate_number <> 1;
+"""
+duckdb_conn.sql(duplicates_removed_query)
 
 # Question: How do you handle missing values by replacing them with 0 in DuckDB?
+missing_values_removed_query = """
+CREATE OR REPLACE TABLE missing_values_removed AS
+SELECT
+    customer_id,
+    customer_name,
+    COALESCE(age, 0) AS Age,
+    gender,
+    COALESCE(purchase_amount, 0.0) AS Purchase_Amount,
+    purchase_date
+FROM duplicates_removed
+"""
+duckdb_conn.execute(missing_values_removed_query)
 
 # Question: How do you remove outliers (e.g., age > 100 or purchase amount > 1000) in DuckDB?
+outliers_removed_query = """
+CREATE OR REPLACE TABLE outliers_removed AS
+SELECT *
+FROM missing_values_removed
+WHERE age <= 100 AND purchase_amount <= 1000;
+"""
+duckdb_conn.execute(outliers_removed_query)
 
 # Question: How do you convert the Gender column to a binary format (0 for Female, 1 for Male) in DuckDB?
+gender_converted_query = """
+CREATE OR REPLACE TABLE gender_converted AS
+SELECT customer_id, customer_name, age,
+    CASE
+        WHEN gender = 'Male' THEN 1
+        WHEN gender = 'Female' THEN 0
+    END AS gender, purchase_amount, purchase_date
+FROM outliers_removed;
+"""
+duckdb_conn.execute(gender_converted_query)
 
 # Question: How do you split the Customer_Name column into separate First_Name and Last_Name columns in DuckDB?
+name_split_query = """
+CREATE OR REPLACE TABLE name_split AS
+SELECT
+    customer_id,
+    SPLIT_PART(customer_name, ' ', 1) AS first_name,
+    SPLIT_PART(customer_name, ' ', 2) AS last_name,
+    age,
+    gender,
+    purchase_amount,
+    purchase_date
+FROM outliers_removed;
+"""
+duckdb_conn.execute(name_split_query)
 
 # Question: How do you calculate the total purchase amount by Gender in DuckDB?
+total_purchase_by_gender_query = """
+CREATE OR REPLACE TABLE total_purchase_by_gender AS
+SELECT
+    gender,
+    SUM(purchase_amount) AS total_purchase
+FROM name_split
+GROUP BY gender;
+"""
+duckdb_conn.execute(total_purchase_by_gender_query)
 
 # Question: How do you calculate the average purchase amount by Age group in DuckDB?
+average_purchase_by_age_group_query = """
+CREATE OR REPLACE TABLE average_purchase_by_age_group AS
+SELECT
+    CASE
+        WHEN 18 <= age and age <= 30 THEN '18-30'
+        WHEN age <= 40 THEN '31-40'
+        WHEN age <= 50 THEN '41-50'
+        WHEN age <= 60 THEN '51-60'
+        WHEN age <= 70 THEN '61-70'
+        ELSE 'Unexpected'
+    END AS age_group,
+    AVG(purchase_amount) AS average_purchase
+FROM name_split
+GROUP BY 1
+ORDER BY 1;
+"""
+duckdb_conn.execute(average_purchase_by_age_group_query)
 
 # Question: How do you print the results for total purchase amount by Gender and average purchase amount by Age group in DuckDB?
 print("====================== Results ======================")
 print("Total purchase amount by Gender:")
+duckdb_conn.sql("SELECT * FROM total_purchase_by_gender;")
 print("Average purchase amount by Age group:")
+duckdb_conn.sql("SELECT * FROM average_purchase_by_age_group;")
+
+duckdb_conn.close()
